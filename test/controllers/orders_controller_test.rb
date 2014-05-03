77b6b5bc402e1fa04bdef7405b91683d0eaa646e
Javier Anselmi <javieranselmi@gmail.com>
2014-05-03 18:03:21 -0300

    Small test fix. Orders worning, test pass

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -12,6 +12,10 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    LineItem.create(:cart => cart, :product => products(:ruby))
     get :new
     assert_response :success
   end
@@ -53,12 +57,5 @@ class OrdersControllerTest < ActionController::TestCase
     assert_equal flash[:notice], 'Your cart is empty'
   end
 
-  test "should get new" do
-    cart = Cart.create
-    session[:cart_id] = cart.id
-    LineItem.create(:cart => cart, :product => products(:ruby))
-    get :new
-    assert_response :success
-  end
 
 end
