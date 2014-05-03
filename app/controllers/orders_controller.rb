318ad8727df9410b17256a6d6f7a82a34310e5e7
Javier Anselmi <javieranselmi@gmail.com>
2014-05-03 18:55:15 -0300

    Emain sending works

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,6 +42,7 @@ class OrdersController < ApplicationController
 
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        Notifier.order_received(@order).deliver
         format.html { redirect_to(store_url, :notice =>
         'Thank you for your order.' ) }      
         format.json { render :show, status: :created, location: @order }
